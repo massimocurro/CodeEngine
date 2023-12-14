13b9fdc587996e7d21755afcd4c01ea461f94f72
Massimo Curro <m.curro@gruppostratos.com>
2023-12-14 14:06:29 +0100

    FIX

--- codeEngineFunction.py
+++ codeEngineFunction.py
@@ -3,6 +3,7 @@ import requests
 import json
 from cryptography.fernet import Fernet
 
+
 def main(dict):
     headers = {"Content-Type": "application/json"}
     response = requests.post("https://isp-dev01.op.ibmcloud.com/grc/ext/LDCImport/data", headers=headers, auth=("U0L9010", "Intesa2022!"), data=json.dumps([{'fileName': "Pandora_DL_OP_20231103170757.csv", 'group':"Default"}]))
