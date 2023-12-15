22edcd23cf7cab59262b1d77b5dff36d997e0603
Massimo Curro <m.curro@gruppostratos.com>
2023-12-15 14:28:13 +0100

    FIX

--- codeEngineFunction.py
+++ codeEngineFunction.py
@@ -2,9 +2,6 @@ import sys
 import json
 import pip._vendor.requests
 
-
-
 def main(dict):
-    headers = {"Content-Type": "application/json"}
-    response = pip._vendor.requests.get("https://function-76.1at6rgz00yjr.eu-de.codeengine.appdomain.cloud")
+    response = pip._vendor.requests.get("https://function-76.1at6rgz00yjr.eude.codeengine.appdomain.cloud")
     return {'message': response.text}
