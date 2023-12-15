7153c5192a9f03eb2b81dccdb42e523a3f326ac4
Massimo Curro <m.curro@gruppostratos.com>
2023-12-15 17:25:32 +0100

    FIX

--- codeEngineFunction.py
+++
@@ -1,9 +0,0 @@
-import sys
-import json
-import pip._vendor.requests
-from flask import Flask
-
-@app.route("/")
-def defineCall():
-    response = pip._vendor.requests.get('https://function-76.1at6rgz00yjr.eude.codeengine.appdomain.cloud')
-    return {'message': response.text}
