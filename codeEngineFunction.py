7d04669e76e42f5e75ba226dd5e8b074100b2a65
Massimo Curro <m.curro@gruppostratos.com>
2023-12-15 14:22:30 +0100

    FIC

--- codeEngineFunction.py
+++ codeEngineFunction.py
@@ -2,10 +2,9 @@ import sys
 import json
 import pip._vendor.requests
 
-app = Flask(__name__)
 
 @app.route("/")
-def callFunction(dict):
+def callFunction():
     headers = {"Content-Type": "application/json"}
     response = pip._vendor.requests.get("https://function-76.1at6rgz00yjr.eu-de.codeengine.appdomain.cloud")
     return {'message': response.text}
