173a3388417f494d47d3f69e55405b92d7ff5606
Massimo Curro <m.curro@gruppostratos.com>
2023-12-17 09:25:55 +0100

    FIX

--- run.py
+++
@@ -1,5 +0,0 @@
-import pip._vendor.requests 
-
-def main(dict):
-    response = pip._vendor.requests.get('https://function-76.1at6rgz00yjr.eu-de.codeengine.appdomain.cloud')
-    return {'message': response.text}
