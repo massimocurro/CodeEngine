ed8910a4b5931bfed55a471364fc5e715829ed49
Massimo Curro <m.curro@gruppostratos.com>
2023-12-17 09:19:57 +0100

    FIX

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 import pip._vendor.requests 
-import json
 
 def main(dict):
-    headers = {"Content-Type": "application/json"}
     response = pip._vendor.requests.get('https://function-76.1at6rgz00yjr.eu-de.codeengine.appdomain.cloud')
     return {'message': response.text}
