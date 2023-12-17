ce43918007d602843235ce772559c9e88d530705
Massimo Curro <m.curro@gruppostratos.com>
2023-12-17 09:12:55 +0100

    FIX

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-#!/bin/bash
+import pip._vendor.requests 
+import json
 
-curl https://function-76.1at6rgz00yjr.eude.codeengine.appdomain.cloud
+def main(dict):
+    headers = {"Content-Type": "application/json"}
+    response = pip._vendor.requests.get('https://function-76.1at6rgz00yjr.eu-de.codeengine.appdomain.cloud')
+    return {'message': response.text}
